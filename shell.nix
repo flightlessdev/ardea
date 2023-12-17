cc890aa78edc483d0aaad655356d2ed379e879dc
Erik <erik@flightless.dev>
2023-12-17 16:38:16 +0100

    Add nix-shell

---
+++ shell.nix
@@ -0,0 +1,28 @@
+with (import <nixpkgs> {
+
+});
+mkShell {
+    buildInputs = [
+        erlangR26
+        elixir_1_15
+        rebar3
+
+
+    ];
+    HISTFILE = "${toString ./.}/.bash_history";
+
+    LANG = "C.UTF-8";
+    LC_ALL = "C.UTF-8";
+    shellHook = ''
+        mkdir -p .mix
+        mkdir -p .hex
+        export MIX_HOME=$PWD/.mix
+        export MIX_ARCHIVES=$MIX_HOME/archives
+        export HEX_HOME=$PWD/.hex
+        export PATH=$MIX_HOME/bin:$HEX_HOME/bin:$PATH
+        export BUILD_WITHOUT_QUIC=1 # For exmqtt dep
+        export ERL_AFLAGS="-kernel shell_history enabled"
+        mix archive.install --force github hexpm/hex tag v2.0.5
+        mix local.rebar rebar3 --force ${pkgs.rebar3}/bin/rebar3
+'';
+}
